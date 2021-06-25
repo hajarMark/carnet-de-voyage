71920909224fa16ed5d435759023ee8db8284faa
markaouiha <hajar.markaoui@natixis.com>
2021-06-25 09:10:57 +0200

    Project initialisation

---
+++ src/main/java/fr/wildcode/school/com/carnet/de/voyage/CarnetDeVoyageApplication.java
@@ -0,0 +1,13 @@
+package fr.wildcode.school.com.carnet.de.voyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnetDeVoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnetDeVoyageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/wildcode/school/com/carnet/de/voyage/CarnetDeVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.wildcode.school.com.carnet.de.voyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnetDeVoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
